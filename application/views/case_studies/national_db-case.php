f44d6c3c45aa0b8c05faa21b44ba1e5154b69c2e
hadi ali <hadi2516505@gmail.com>
2021-10-01 22:13:23 +0500

    case-pages

--- application/views/case_studies/national_db-case.php
+++ application/views/case_studies/national_db-case.php
@@ -1,8 +1,121 @@
+<div class="main-cont">
+  <div class="main-banner nation-banner">
+    <section class="content for-mobile-padding national-top-banner srv-info-sec ecomm">
+      <div class="container mt5">
+        <div class="row cntr-content">
+          <div class="col-md-6 animate slideInLeft nationalbannercstm">
+            <h2 class="">National Specialist Database Web Development</h2>
+            <p>In the football-crazed United States, National Specialist Database is a nationwide portal for coaches to find the hottest prospects and the best football players for their clubs.
+            </p>
+          </div>
+          <div class="col-md-6 animate national-banner-main-img bounceIn">
+            <div class="EcoImg">
+              <img src="<?= g('images_root') ?>1x/mobile3-8.png" height="100%" width="100%" alt="">
+            </div>
+          </div>
 
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="container">
+    <div class="nation-second-banner">
+      <div class="col-lg-12 first-banner-heading mt8 formobilemargin">
+        <h1>Client brief</h1>
+      </div>
+      <div class="col-lg-12 national-banner-para mt2">
+        <p>The client wanted a portal to be created that would let Realtor's and Cleaner's dreams come true. This platform allows for ease of access on both sides, meaning whoever visits will find the house clean! <br>
+          The portal on the website should let cleaners and Realtors register so they can find their preferred jobs.</p>
+      </div>
+      <div class="col-lg-12 national first-banner-mid mt4">
+        <div class="national-type nation-margin-for-mobile">
+          <div ><span>YEAR</span></div>
+          <div>
+            <p>2021</p>
+          </div>
+        </div>
+        <div class="national-type nation-margin-for-mobile">
+          <div><span>ROLE</span></div>
+          <div>
+            <p>UI/UX</p>
+          </div>
+        </div>
+        <div class="national-type2 nation-margin-for-mobile">
+          <div><span>TYPE</span></div>
+          <div>
+            <p>Web Development</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="national-introduction">
+      <div class="col-lg-12 cstmclass">
+        <span>the introduction</span>
+      </div>
+      <div class="col-lg-12">
+        <div class="natonal-intro-img"> <img src="<?= g('images_root') ?>1x/sectionimg-8.png" height="100%" width="100%" alt="">
+        </div>
+      </div>
+      <div class="col-lg-12">
+        <p>We had to create a website to maintain the database of football players from all over the country that would allow them to get themselves registered based on their skill level.</p>
+        <P>Players no longer depend on the evaluation platforms offered by their high school. This website has it all - from player biographies with statistics and awards received, so coaches can make an informed decision while selecting the player.</p>
 
+      </div>
 
-  <section class="animate slideInUp ecomm">
-    hi
-  </section>
+    </div>
+    <div class="national-problem">
+      <div class="col-lg-12 cstmclass">
+        <span>the problem</span>
+      </div>
+      <div class="col-lg-12">
+        <p>Cyber safety is a central issue in the twenty-first century, so this website needed to be safe. With the extensive data pool and sensitive information to protect, we had to ensure everything from the database to the payment method was secure. The website should feel intuitive and inviting.
+        <P>We welcomed the challenge wholeheartedly.
+        </p>
+        </p>
+      </div>
 
+    </div>
+    <section class="content srv-info-sec ecomm mt8 formobilemargin">
+      <div class="container">
+        <div class="row cntr-content">
+          <div class="col-md-7 animate slideInLeft">
+            <div class="col-lg-12 solution cstmclass">
+              <span>the solution</span>
+            </div>
+            <div class="col-lg-12">
+              <p>It was clear from the beginning that this project wasn’t going to be easy because it involved so much data that had to be made secure.
+                We sat down with our creative developers and designers and, after three brainstorming sessions, came up with the strategy and workflow that would be implemented to create this website.
+                A few weeks of hard work later, we settled onto a website that had everything-from security to interactive UI.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-5  animate bounceIn">
+            <div class="natinal-EcoImg">
+              <img src="<?= g('images_root') ?>1x/nationalmobile-8.png" height="100%" width="100%" alt="">
+            </div>
+          </div>
 
+        </div>
+      </div>
+    </section>
+    <div class="national-problem">
+      <div class="col-lg-12 cstmclass">
+        <span>highlights</span>
+      </div>
+      <div class="col-lg-12">
+        <p>The client’s willingness to accept our suggestions made this project a great treat for us, collaborating with someone who could actually understand the complexity of the project was an absolute delight.
+        </p>
+        <p>Creating a database that would be operational on a national level was a challenge for our team, but challenges are what keeps us going.
+          We developed the whole website but ensured that the client was on board with the designs from time to time to further move into development swiftl.</p>
+      </div>
+
+    </div>
+    <div class="national-problem">
+      <div class="col-lg-12 cstmclass">
+        <span>the results</span>
+      </div>
+      <div class="col-lg-12">
+        <p>The end result was one of the best masterpieces we have created. The website had an interactive UI and was among some of our most secure websites to date.</p>
+      </div>
+    </div>
+    <? $this->load->view('widgets/service_form'); ?>
