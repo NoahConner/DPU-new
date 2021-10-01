65eb9d74c8da432c40cc25e9b9d113a4ded0f976
WahajAhmedIqbal <wahaj465@gmail.com>
2021-10-01 23:10:15 +0500

    case Study

--- application/views/case_studies/phase_dev-case.php
+++ application/views/case_studies/phase_dev-case.php
@@ -1,8 +1,169 @@
+<div class="main-cont">
+  <div class="main-banner phase3Banner">
 
+    <div class="bannerHeading">
+      <h3>
+        Phase 3 kicking
+      </h3>
+      <h3>
+        Web Development
+      </h3>
+      <p>Phase 3 Kicking is designed to give athletes a platform for self-evaluation <br> that meshes with the competition needs of industry and collegiate football <br> teams.</p>
+    </div>
 
+    <div class="bannerfrontimge">
+      <img src=" <?= g('images_root') ?>/1x/Asset 8-8.png" width="100%" height="100%" alt="">
+    </div>
+
+  </div>
 
   <section class="animate slideInUp ecomm">
-    hi
+    <div class="container">
+      <div class="frog-clientBrif frog-clientBrif221 d-flex flex-column align-items-center mt4 mb3">
+        <h1>
+          Client brief
+        </h1>
+        <p>
+          The client had a football coaching club. They wanted to get their players registered on the website so that the self-evaluation on the portal could rate players according to their expertise. This would let coaches who visit the website see players according to their level of expertise and skill set.
+        </p>
+
+        <div class="frogClintBrif d-flex justify-content-between">
+          <div class="box1 box2forcolor">
+            <h5>
+              YEAR
+            </h5>
+            <h2>
+              2021
+            </h2>
+          </div>
+          <div class="box1 box2forcolor">
+
+            <h5>
+              ROLE
+            </h5>
+            <h2>
+              UI/UX
+            </h2>
+          </div>
+          <div class="box1 box2forcolor">
+            <h5>
+              TYPE
+            </h5>
+            <h2>
+              Web Development
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   </section>
 
 
+  <section class="content srv-info-sec ecomm mt5">
+    <div class="container">
+      <div class="row cntr-content">
+        <div class="col-md-8 animate slideInLeft phase3intropara">
+          <div class="heading-sep">
+            <!-- <span class="seperator-left"></span> -->
+          </div>
+          <h2 class="phase3headingStyle">The introduction</h2>
+          <p> We wanted to create a portal that best reflects the Phase 3 kicking business model to keep things simple. We wanted to make sure that football players could be easily registered on the website by the owner. The website should have a sleek and modern feel at the same time and should have great UI/UX.</p>
+        </div>
+        <div class="col-md-4 plpx-70 animate bounceIn dnon">
+          <div class="EcoImg">
+            <img src="<?= g('images_root') ?>1x/phase3IntroImg-8.png" height="100%" width="100%" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="content srv-info-sec ecomm mt5">
+    <div class="container">
+      <div class=" cntr-content">
+        <div class=" animate slideInLeft phase3intropara d-flex flex-column align-items-center">
+          <div class="heading-sep">
+            <!-- <span class="seperator-left"></span> -->
+          </div>
+          <h2 class="phase3headingStyle customphase3headingStyle">The problem</h2>
+          <p class="customparaphase3">The Client wanted that his website should have camp results displayed, drag and drop features for each player, and filters on the website by which coaches can easily navigate the players according to expertise.</p>
+          <p>The website should feature camp results, drag and drop features, and the use of filters on the website. </p>
+          <span>
+            <p>We welcomed the challenge wholeheartedly.</p>
+          </span>
+        </div>
+        <div class=" animate bounceIn dnon">
+          <div class="EcoImg cusEcoImg ">
+            <img src="<?= g('images_root') ?>1x/phase3ProbImg-8.png" height="100%" width="100%" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="content srv-info-sec ecomm mt5">
+    <div class="container">
+      <div class=" cntr-content">
+        <div class=" animate slideInLeft phase3intropara d-flex flex-column align-items-center">
+          <div class="heading-sep">
+            <!-- <span class="seperator-left"></span> -->
+          </div>
+          <h2 class="phase3headingStyle customphase3headingStyle">The solution</h2>
+          <p class="customparaphase33">It was clear from the beginning that our client wanted an impressive and feature-rich website. Our creative team started brainstorming, imagining what we could do together to make it happen.</p>
+
+          <span>
+            <p>A few weeks of hard work later, we settled onto a sleek design incorporating all necessary features for this particular project!</p>
+          </span>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+
+  <section class="content srv-info-sec ecomm mt5">
+    <div class="container">
+      <div class="row cntr-content">
+        <div class="col-md-8 animate slideInLeft phase3intropara">
+          <div class="heading-sep">
+            <!-- <span class="seperator-left"></span> -->
+          </div>
+          <h2 class="phase3headingStyle">Highlights</h2>
+          <p class="highlightsPara"> Countless hours working relentlessly on a project finally proved fruitful when the client appreciated us for our work.</p>
+          <p class="highlightsPara">The client was very focused on his project, which made our job of achieving the goals together a lot easier.</p>
+          <p class="highlightsPara">We created the whole website from scratch and ensured that all the features were added to the website. The landing pages were carefully crafted maintaining the site’s overall theme.</p>
+        </div>
+        <div class="col-md-4 plpx-70 animate bounceIn dnon">
+          <div class="EcoImg">
+            <img src="<?= g('images_root') ?>1x/phase3highlihtsImg-8.png" height="100%" width="100%" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="content srv-info-sec ecomm mt5 mb2">
+    <div class="container">
+      <div class=" cntr-content">
+        <div class=" animate slideInLeft phase3intropara d-flex flex-column align-items-center">
+          <div class="heading-sep">
+            <!-- <span class="seperator-left"></span> -->
+          </div>
+          <h2 class="phase3headingStyle customphase3headingStyle">The results</h2>
+          <p class="customparaphase330">The website had all the features added to it and looked excellent ensuring a great user experience.
+          </p>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<? $this->load->view('widgets/service_form'); ?>
